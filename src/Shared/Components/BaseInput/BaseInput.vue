af79ddfcc816cb41933fb0e85a76415c580e0800
ryinner <vinograd18062002@gmail.com>
2022-12-15 23:26:26 +0500

    base input

---
+++ src/Shared/Components/BaseInput/BaseInput.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="input">
+        <input class="input__field" v-bind="inputAttrs" />
+        <label v-if="inputAttrs.placeholder?.length > 0" class="input__label">{{inputAttrs.placeholder}}</label>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    inputAttrs: {
+        type: Object,
+        required: false,
+        default: () => ({})
+    },
+})
+</script>
+
+<style lang="scss">
+@import "@/Shared/Assets/Scss/vars";
+@import "@/Shared/Assets/Scss/fonts";
+@import "@/Shared/Assets/Scss/indention";
+@import "@/Shared/Assets/Scss/touch";
+.input {
+    position: relative;
+
+    &__label {
+        top: -6px;
+        display: block;
+        transition: 0.2s;
+        position: absolute;
+        color: $black-color;
+        @include no-touch;
+        @include inter-12-bold;
+    }
+    &__field {
+        margin-top: 2px;
+        outline: 0;
+        border: 0;
+        width: 100%;
+        border-bottom: 1px solid $gray-color;
+        background: transparent;
+        transition: border-color 0.2s;
+        @include padding-vertical(7px);
+        @include inter-14-regular;
+
+        &::placeholder {
+            color: $gray-color;
+        }
+        &:placeholder-shown ~ .input__label {
+            color: $gray-color;
+            top: 8px;
+            @include inter-14-regular;
+        }
+        &:focus {
+            color: $black-color;
+            padding-bottom: 6px;
+            border-width: 2px;
+            border-color: $primary-color;
+
+            & ~ .input__label {
+                color: $black-color;
+                top: -6px;
+                @include inter-12-bold;
+            }
+        }
+    }
+}
+</style>
--- src/Shared/Components/Layout/TheUser/TheUser.vue
+++ src/Shared/Components/Layout/TheUser/TheUser.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class="user">
+    <div class="user" @click="isModalOpen = true">
         <picture class="user__image">
             <svg class="user__icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 15C15.3137 15 18 12.3137 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 12.3137 8.68629 15 12 15Z" stroke-width="1.5" stroke-miterlimit="10"/>
                 <path d="M2.90527 20.2491C3.82736 18.6531 5.15322 17.3278 6.74966 16.4064C8.34611 15.485 10.1569 15 12.0002 15C13.8434 15 15.6542 15.4851 17.2506 16.4065C18.8471 17.3279 20.1729 18.6533 21.0949 20.2493" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
         </picture>
+        <BaseModal :isShow="isModalOpen" class="auth registration" @close="isModalOpen = false">
+            <template #heading><h4 class="auth__header">Войти</h4></template>
+            <template #body>
+                <form @submit.prevent="">
+                    <BaseInput :inputAttrs="{name: 'login', type: 'text', placeholder: 'Логин'}" />
+                </form>
+            </template>
+        </BaseModal>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import BaseInput from "../../BaseInput/BaseInput.vue";
+import BaseModal from '../../BaseModal/BaseModal.vue';
 
+const isModalOpen = ref(false);
 </script>
 
 <style lang="scss">
@@ -22,5 +34,8 @@
     &__icon {
         @include icon-base-style;
     }
+}
+.auth {
+
 }
 </style>
