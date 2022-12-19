dcb7af0a14defafd00551a76acb47ad546c7b435
ryinner <vinograd18062002@gmail.com>
2022-12-19 23:38:32 +0500

    pulse lazy load effect

--- src/Shared/Components/BaseLoading/BaseLoading.vue
+++ src/Shared/Components/BaseLoading/BaseLoading.vue
@@ -11,6 +11,18 @@
 .loading {
     width: 100%;
     height: 100%;
-    background-color: $gray-color;
+    animation: loading-pulse 1.8s ease-in-out infinite alternate;
+
+    @keyframes loading-pulse {
+        0% {
+            background: $gray-color;
+        }
+        50% {
+            background: $lighter-gray-color;
+        }
+        100% {
+            background: $gray-color;
+        }
+    }
 }
 </style>
